a34a5554ac7404edd1c734dd8c826bbc36f51870
Martin Goldblat <mgoldblat@mercadolibre.com>
2019-08-09 17:18:53 -0300

    Make it works with guice

--- src/main/java/com/mercadolibre/bootstrap/App.java
+++ src/main/java/com/mercadolibre/bootstrap/App.java
@@ -1,32 +1,19 @@
 package com.mercadolibre.bootstrap;
 
 import com.mercadolibre.controllers.SearchController;
-import com.mercadolibre.bootstrap.service.SearchService;
 import io.jooby.Jooby;
-import io.jooby.di.SpringModule;
-//import io.jooby.di.GuiceModule;
+import io.jooby.di.GuiceModule;
 
 
 public class App extends Jooby {
 
     {
-        //install(new GuiceModule());
-        install(new SpringModule());
-
-        get("/search-service", ctx -> {
-            SearchService service = require(SearchService.class);
-            return service.Search();
-        });
+        install(new GuiceModule());
 
         mvc(SearchController.class);
     }
 
     public static void main(String[] args) {
-        // TODO Investigate https://jooby.io/#execution-model
-
-        //var context = (ApplicationContext)new ClassPathXmlApplicationContext("applicationContext.xml");
-
-
         runApp(args, App::new);
     }
 }
--- src/main/java/com/mercadolibre/controllers/SearchController.java
+++ src/main/java/com/mercadolibre/controllers/SearchController.java
@@ -2,8 +2,6 @@ package com.mercadolibre.controllers;
 
 import io.jooby.annotations.GET;
 import io.jooby.annotations.Path;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 
 @Path("/search")
 public class SearchController {
